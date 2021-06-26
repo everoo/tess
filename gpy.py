5423837fdfb9a9b795278135982671333d995e07
everoo <37887717+everoo@users.noreply.github.com>
2021-06-26 07:32:37 -0700

    Update gpy.py

--- gpy.py
+++ gpy.py
@@ -1,40 +1,41 @@
+all_points = [(0, 0)]
+
 class Tree:
-    def __init__(self, point, ticket):
+    def __init__(self, path, ticket):
+        self.path = path
         self.ticket = ticket
-        self.point = point
         self.children = []
-        self.path = []
 
-    def createChild(self, newPoint, hasTicket):
-        child = Tree(newPoint, hasTicket)
-        child.path = self.path+[self.point]
-        self.children.append(child)
+    def move(self, dir):
+        return self.path[-1][0]+dir[0], self.path[-1][1]+dir[1]
 
-    def move_self(self, dir):
-        return self.point[0]+dir[0], self.point[1]+dir[1]
+    def addChild(self, nextPoint, hasTicket):
+        all_points.append(nextPoint)
+        self.children.append(Tree(self.path+[nextPoint], hasTicket))
 
     def find_children(self, maze):
+        wh = len(maze[-1])-1, len(maze)-1
         for direction in (0, 1), (1, 0), (0, -1), (-1, 0):
-            nextPoint = self.move_self(direction)
-            if nextPoint == (len(maze[-1])-1, len(maze)-1):
-                return self.path+[self.point, nextPoint]
-            elif nextPoint not in self.path:
+            nextPoint = self.move(direction)
+            if nextPoint == wh:
+                return self.path+[nextPoint]
+            elif nextPoint not in self.path and nextPoint not in all_points:
                 value = get_value(maze, nextPoint)
                 if value == 0:
-                    self.createChild(nextPoint, self.ticket)
+                    self.addChild(nextPoint, self.ticket)
                 elif value == 1 and self.ticket:
-                    self.createChild(nextPoint, False)
+                    self.addChild(nextPoint, False)
 
 def get_value(m, (x, y)):
     if 0 <= y < len(m) and 0 <= x < len(m[0]):
         return m[y][x]
 
 def solution(m):
-    queue = [Tree((0, 0), True)]
-    path = None
-    while len(queue) > 0 and path == None:
+    queue = [Tree([(0, 0)], True)]
+    while len(queue) > 0:
         path = queue[0].find_children(m)
         if path == None:
             queue += queue[0].children
             del queue[0]
-    return path
+        else:
+            return path
