b7e0fc4f956d3e0400fdce55fc5ef0d7417c1527
everoo <37887717+everoo@users.noreply.github.com>
2021-06-26 08:26:33 -0700

    Update gpy.py

--- gpy.py
+++ gpy.py
@@ -1,4 +1,6 @@
-all_points = [(0, 0)]
+def get_value(m, (x, y)):
+    if 0 <= y < len(m) and 0 <= x < len(m[-1]):
+        return m[y][x]
 
 class Tree:
     def __init__(self, path, ticket):
@@ -10,29 +12,33 @@ class Tree:
         return self.path[-1][0]+dir[0], self.path[-1][1]+dir[1]
 
     def addChild(self, nextPoint, hasTicket):
-        all_points.append(nextPoint)
+        # all_points.append(nextPoint)
         self.children.append(Tree(self.path+[nextPoint], hasTicket))
 
     def find_children(self, maze):
-        wh = len(maze[-1])-1, len(maze)-1
         for direction in (0, 1), (1, 0), (0, -1), (-1, 0):
             nextPoint = self.move(direction)
-            if nextPoint == wh:
+            if nextPoint == (len(maze[-1])-1, len(maze)-1):
                 return self.path+[nextPoint]
-            elif nextPoint not in self.path and nextPoint not in all_points:
+            elif nextPoint not in self.path:
                 value = get_value(maze, nextPoint)
                 if value == 0:
                     self.addChild(nextPoint, self.ticket)
                 elif value == 1 and self.ticket:
                     self.addChild(nextPoint, False)
 
-def get_value(m, (x, y)):
-    if 0 <= y < len(m) and 0 <= x < len(m[0]):
-        return m[y][x]
-
 def solution(m):
     queue = [Tree([(0, 0)], True)]
     while len(queue) > 0:
+        if len(queue) > 400:
+            new_queue = {}
+            for t in queue:
+                key = t.path[-1], t.ticket
+                if key not in new_queue:
+                    new_queue[key] = t
+                elif len(t.path) < len(new_queue[key].path):
+                    new_queue[key] = t
+            queue = new_queue.values()
         path = queue[0].find_children(m)
         if path == None:
             queue += queue[0].children
